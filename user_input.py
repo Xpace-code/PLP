692cc10b7c5ed4b41cf276843ff1fa75f70ee563
Xpace-code <auduemmanuel624@gmail.com>
2024-11-07 15:10:14 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What's your name?")
+age = input("What's your age?")
+location = input("What's your location?")
+print
+(
+"Hello Emmanuel, you are 22 years old and live in California"
+)
